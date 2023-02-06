50c25a16fb106c440fe71effcdf0edf79816e660
happyman <happyman.eric@gmail.com>
2023-02-06 22:37:42 +0800

    for 1.0 version

---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# test build
+./twmap.sh -d -r 236:2514:1:1:TWD67 -G -t test1 -v 2016
+./twmap.sh -d -r 236:2514:1:1:TWD67 -G -t test1 -v 3
+./twmap.sh -d -r 236:2514:1:1:TWD67 -G -t test1 -v 1
